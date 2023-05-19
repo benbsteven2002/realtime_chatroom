aed5252011d1bf28953483fcda0dcfa88690b9cb
Benjamin Steven <benjamin@Benjamins-MacBook-Air-3.local>
2023-05-19 09:41:38 +0200

    A very basic chatroom

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # realtime_chatroom
 Client-server chatroom
+
+1. Basic chat room
+2. Chat room functionality with whisper, user list ect
+3. File transfer?
+4. VoIP?
+5. Security?
--- server.js
+++ server.js
@@ -1,11 +1,29 @@
 const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+
 const app = express();
-const server = require('http').createServer(app);
-const io = require('socket.io')(server);
+const server = http.createServer(app);
+const io = socketIO(server);
 
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
 
-const port = 3000;
-server.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  socket.on('message', (data) => {
+    io.emit('message', data); // Broadcast the message to everyone
+  });
+
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+  });
+});
+
+server.listen(3000, () => {
+  console.log('Server is running on port 3000');
 });
