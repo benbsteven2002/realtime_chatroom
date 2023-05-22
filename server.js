92087c165fa02a1e868c466d83ab030a38a8708d
Benjamin Steven <benjamin@Benjamins-MacBook-Air-3.local>
2023-05-22 21:13:20 +0200

    Colour

--- server.js
+++ server.js
@@ -15,14 +15,30 @@ app.get('/', (req, res) => {
 io.on('connection', (socket) => {
   console.log('A user connected');
 
-  socket.on('message', (data) => {
-    io.emit('message', data); // Broadcast the message to everyone
+  // Handle setting username
+  socket.on('setUsername', (username) => {
+    // Store the username in the socket object
+    socket.username = username;
+    console.log(`User ${username} connected`);
+
+    // Notify other clients that a new user has joined
+    socket.broadcast.emit('userJoined', username);
   });
 
+  // Handle incoming messages
+  socket.on('message', (data) => {
+    // Broadcast the message to everyone
+    io.emit('message', { username: socket.username, message: data });
+  });
+
+  // Handle user disconnection
   socket.on('disconnect', () => {
-    console.log('A user disconnected');
+    console.log(`User ${socket.username} disconnected`);
+    // Notify other clients that a user has left
+    socket.broadcast.emit('userLeft', socket.username);
   });
 });
+
 
 server.listen(3000, () => {
   console.log('Server is running on port 3000');
