2045791eca299d8c5b2348bec02de78171c5aaba
Benjamin Steven <benjamin@Benjamins-MacBook-Air-3.local>
2023-05-23 10:38:07 +0200

    User list + login landing

--- server.js
+++ server.js
@@ -6,23 +6,32 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
 
-app.use(express.static(__dirname + '/public'));
+const users = new Set();
+
+app.use(express.static(__dirname));
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', (socket) => {
-  console.log('A user connected');
+  console.log('A user session started:');
 
-  // Handle setting username
-  socket.on('setUsername', (username) => {
-    // Store the username in the socket object
-    socket.username = username;
-    console.log(`User ${username} connected`);
+  socket.on('login', (username) => {
+    console.log(`We have a login request ${username}`);
 
-    // Notify other clients that a new user has joined
-    socket.broadcast.emit('userJoined', username);
+    if (users.has(username)) {
+      socket.emit('badLogin');
+    } else {
+      socket.emit('loginSuccess');
+      socket.username = username;
+      users.add(username);
+      console.log(`User ${username} connected`);
+      // Notify other clients that a new user has joined
+      socket.broadcast.emit('userJoined', username);
+      // Send the updated list of online users to all clients
+      io.emit('onlineUsers', Array.from(users));
+    }
   });
 
   // Handle incoming messages
@@ -33,12 +42,16 @@ io.on('connection', (socket) => {
 
   // Handle user disconnection
   socket.on('disconnect', () => {
-    console.log(`User ${socket.username} disconnected`);
-    // Notify other clients that a user has left
-    socket.broadcast.emit('userLeft', socket.username);
+    if (socket.username) {
+      console.log(`User ${socket.username} disconnected`);
+      users.delete(socket.username);
+      // Notify other clients that a user has left
+      socket.broadcast.emit('userLeft', socket.username);
+      // Update the list
+      io.emit('onlineUsers', Array.from(users));
+    }
   });
 });
-
 
 server.listen(3000, () => {
   console.log('Server is running on port 3000');
